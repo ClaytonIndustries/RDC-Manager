26b6a5252adf53692bbf505d33d94833ff905fa6
Michael Clayton <mike-16@hotmail.co.uk>
2017-10-12 23:17:12 +0100

    Add settings screen

---
+++ Source/RDCManager/Models/UserAccount.cs
@@ -0,0 +1,21 @@
+﻿
+namespace RDCManager.Models
+{
+    public class UserAccount
+    {
+        public string Name
+        {
+            get; set;
+        }
+
+        public string Username
+        {
+            get; set;
+        }
+
+        public string Password
+        {
+            get; set;
+        }
+    }
+}
--- Source/RDCManager/ViewModels/RDCSettingsViewModel.cs
+++ Source/RDCManager/ViewModels/RDCSettingsViewModel.cs
@@ -1,8 +1,33 @@
-﻿using Caliburn.Micro;
+﻿using System.Collections.ObjectModel;
+using Caliburn.Micro;
+using RDCManager.Models;
 
 namespace RDCManager.ViewModels
 {
     public class RDCSettingsViewModel : Screen
     {
+        private ObservableCollection<UserAccount> _accounts;
+        public ObservableCollection<UserAccount> Accounts
+        {
+            get { return _accounts; }
+            private set { _accounts = value; NotifyOfPropertyChange(() => Accounts); }
+        }
+
+        private UserAccount _selectedAccount;
+        public UserAccount SelectedAccount
+        {
+            get { return _selectedAccount; }
+            set { _selectedAccount = value; NotifyOfPropertyChange(() => SelectedAccount); }
+        }
+
+        public RDCSettingsViewModel()
+        {
+            Accounts = new ObservableCollection<UserAccount>();
+        }
+
+        public void NewAccount()
+        {
+            Accounts.Add(new UserAccount());
+        }
     }
 }
