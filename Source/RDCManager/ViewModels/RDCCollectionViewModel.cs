aa08310912dc39949dcfd5df39de49b89ec22d30
Michael Clayton <michael.clayton@royalmail.com>
2017-10-18 12:10:28 +0100

    Filter by rdc group on collection screen

--- Source/RDCManager/ViewModels/RDCCollectionViewModel.cs
+++ Source/RDCManager/ViewModels/RDCCollectionViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Data;
 using Caliburn.Micro;
 using RDCManager.Messages;
 using RDCManager.Models;
@@ -21,18 +25,54 @@ namespace RDCManager.ViewModels
             set { _selectedRDC = value; NotifyOfPropertyChange(() => SelectedRDC); }
         }
 
+        private ObservableCollection<RDCGroup> _rdcGroups;
+        public ObservableCollection<RDCGroup> RDCGroups
+        {
+            get { return _rdcGroups; }
+            private set { _rdcGroups = value; NotifyOfPropertyChange(() => RDCGroups); }
+        }
+
+        private RDCGroup _selectedRDCGroup;
+        public RDCGroup SelectedRDCGroup
+        {
+            get { return _selectedRDCGroup; }
+            set { _selectedRDCGroup = value; _groupView?.Refresh(); NotifyOfPropertyChange(() => SelectedRDCGroup); }
+        }
+
         private readonly IEventAggregator _events;
         private readonly IRDCInstanceManager _rdcInstanceManager;
+        private readonly IRDCGroupManager _rdcGroupManager;
 
-        public RDCCollectionViewModel(IEventAggregator events, IRDCInstanceManager rdcInstanceManager)
+        private ICollectionView _groupView;
+
+        public RDCCollectionViewModel(IEventAggregator events, IRDCInstanceManager rdcInstanceManager, IRDCGroupManager rdcGroupManager)
         {
             _events = events;
             _rdcInstanceManager = rdcInstanceManager;
+            _rdcGroupManager = rdcGroupManager;
         }
 
         protected override void OnActivate()
         {
             RDCs = new ObservableCollection<RDC>(_rdcInstanceManager.GetRDCs());
+            RDCGroups = new ObservableCollection<RDCGroup>(_rdcGroupManager.GetGroups());
+
+            RDCGroups.Insert(0, new RDCGroup() { Name = "All", Id = Guid.Empty });
+
+            _groupView = CollectionViewSource.GetDefaultView(_rdcs);
+            _groupView.Filter += (item) =>
+            {
+                RDC rdc = item as RDC;
+
+                if (SelectedRDCGroup == null || SelectedRDCGroup.Id == Guid.Empty || rdc.GroupId == SelectedRDCGroup.Id)
+                {
+                    return true;
+                }
+
+                return false;
+            };
+
+            SelectedRDCGroup = RDCGroups.First();
         }
 
         public void RDCSelected()
